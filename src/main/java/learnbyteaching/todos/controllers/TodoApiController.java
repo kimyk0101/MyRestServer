a6ef03cfef15a6d6bda27dcaba499fa4ad72521a
kimyk0101 <kimyk7131@gmail.com>
2025-01-24 15:05:50 +0900

    POST

--- src/main/java/learnbyteaching/todos/controllers/TodoApiController.java
+++ src/main/java/learnbyteaching/todos/controllers/TodoApiController.java
@@ -2,12 +2,17 @@ package learnbyteaching.todos.controllers;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import learnbyteaching.todos.repositories.dao.TodoRepository;
@@ -36,5 +41,25 @@ public class TodoApiController  {
 		Optional<TodoItem> todo = todoRepository.findById(id);
 		return todo.map(ResponseEntity::ok)
 					.orElseGet(() -> ResponseEntity.notFound().build());
+	}
+	
+	//	keyword 파라미터 전달 받아서 title을 대상으로 검색 -> 목록
+	@GetMapping("/search")
+	public ResponseEntity<List<TodoItem>> searchTodos(@RequestParam("keyword") String keyword) {
+		List<TodoItem> foundTodos = todoRepository.findAll().stream()
+					.filter(todo -> todo.getTitle().toLowerCase()
+					.contains(keyword.toLowerCase()))
+					.collect(Collectors.toList());
+		
+		return ResponseEntity.ok(foundTodos);
+	}
+	
+	//	새로운 TodoItem 생성
+	@PostMapping
+	public  ResponseEntity<TodoItem> createTodo(@RequestBody TodoItem todoItem) {
+		TodoItem savedTodo = todoRepository.save(todoItem);
+		return ResponseEntity.status(HttpStatus.CREATED)
+					.header("Location", "/api/todos/" + savedTodo.getId()).body(savedTodo);
+		//	TODO 나중에 수정
 	}
 }
